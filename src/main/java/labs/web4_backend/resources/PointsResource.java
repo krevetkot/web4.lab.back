b0a68fa6d342978eaefe7dcdce86b0d2682fbb11
krevetkot <ksenia.slonimskaya@yandex.ru>
2025-01-07 00:45:52 +0300

    +insert Point

--- src/main/java/labs/web4_backend/resources/PointsResource.java
+++ src/main/java/labs/web4_backend/resources/PointsResource.java
@@ -3,8 +3,10 @@ package labs.web4_backend.resources;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import labs.web4_backend.model.Point;
 import labs.web4_backend.model.User;
 import labs.web4_backend.utils.DatabaseManager;
+import labs.web4_backend.utils.Validator;
 import org.json.JSONObject;
 
 import javax.net.ssl.HttpsURLConnection;
@@ -12,8 +14,10 @@ import javax.net.ssl.HttpsURLConnection;
 @Path("/points")
 public class PointsResource {
     private final DatabaseManager dbManager;
+    private final Validator validator;
     public PointsResource(){
         dbManager = DatabaseManager.getInstance();
+        validator = new Validator();
     }
 
     @GET
@@ -24,4 +28,24 @@ public class PointsResource {
         response.put("data", dbManager.getPoints());
         return Response.ok(response.toString(), MediaType.APPLICATION_JSON).build();
     }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response insertPoint(String body) {
+
+        JSONObject request = new JSONObject(body);
+        float x = Float.parseFloat(request.optString("x"));
+        float y = Float.parseFloat(request.optString("y"));
+        float r = Float.parseFloat(request.optString("r"));
+        boolean isHit = validator.isHit(x, y, r);
+        Point point = new Point(x, y, r, isHit);
+
+        dbManager.insertPoint(point);
+
+        JSONObject response = new JSONObject();
+        response.put("status", HttpsURLConnection.HTTP_OK);
+        return Response.ok(response.toString(), MediaType.APPLICATION_JSON).build();
+    }
+
 }
---
+++ src/main/java/labs/web4_backend/utils/Validator.java
@@ -0,0 +1,10 @@
+package labs.web4_backend.utils;
+
+public class Validator {
+    public boolean isHit(float x, float y, float r) {
+        return ((x >= 0) && (x <= r) && (y >= 0) && (y <= r) || //in rectangle
+                (x>=0) && (y >= x/2 - (r/2)) && (y <= 0) || //in triangle
+                (x * x + y * y <= r * r ) && (x <= 0) && (y <= 0) //in circle
+        );
+    }
+}
