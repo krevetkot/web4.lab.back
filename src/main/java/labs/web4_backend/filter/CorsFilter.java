d7a1e7fb0a4271016b63b8168d230b5740b15023
krevetkot <ksenia.slonimskaya@yandex.ru>
2025-01-12 17:14:34 +0300

    add 'no-cache' header

--- src/main/java/labs/web4_backend/filter/CorsFilter.java
+++ src/main/java/labs/web4_backend/filter/CorsFilter.java
@@ -27,10 +27,11 @@ public class CorsFilter implements Filter {
         HttpServletRequest req = (HttpServletRequest) request;
 
         // Установим CORS-заголовки
-        res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+        res.setHeader("Access-Control-Allow-Origin", "http://k-sl.ru");
         res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
         res.setHeader("Access-Control-Allow-Credentials", "true");
+        res.setHeader("Cache-Control", "no-cache");
 
         // Прерываем preflight-запрос (OPTIONS)
         if ("OPTIONS".equalsIgnoreCase(req.getMethod())) {
